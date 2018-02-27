b325463cafda032076293d3c9b8f93858f89a6f7
TCXP1 <carolyn_peters@unigroup.com>
2018-02-27 08:33:09 -0600

    filtering works

--- src/App.js
+++ src/App.js
@@ -13,20 +13,32 @@ class App extends Component {
    .then( responce => responce.json() )
    .then( ({results: concerts}) => this.setState({concerts}))
   }
-   
+  
+  filter(event){
+    this.setState({filter: event.target.value})
+  }
+
   render(){
-      let concerts = this.state.concerts
+      let concerts = this.state.concerts;
+      if(this.state.filter){
+        concerts = concerts.filter(concert => concert.eventDateName.toLowerCase()
+        .includes(this.state.filter.toLowerCase()))
+      }
+
       return (
         <div>
-          {concerts.map((aConcert, i)=> 
+          <input type="text" onChange={this.filter.bind(this)}/>
+            {concerts.map((aConcert, i)=> 
             <div key={i}>
+            <img src={aConcert.imageSource}></img>
+            <div>{aConcert.dateOfShow}</div>
             <div>{aConcert.name}</div>
             <div>{aConcert.eventDateName}</div>
-            </div>)}
+            <div>{aConcert.eventHallName}</div>
+            </div>)} 
         </div>
-      );
+      )
   }
 }
-
 
 export default App;
